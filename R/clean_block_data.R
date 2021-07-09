82b5235b86d46d4a5ddfd18600b434522a9c760e
Bertrand Wilden <bert@bwilden.com>
2021-07-09 14:09:22 -0500

    changed variable name to "state_code"

--- R/clean_block_data.R
+++ R/clean_block_data.R
@@ -11,13 +11,13 @@ blocks_1990 <- read_csv(here("data-raw", "nhgis_ds120_1990_block.csv")) %>%
          api = ET2004,
          other = ET2005,
          hispanic = ET2006 + ET2007 + ET2008 + ET2009 + ET2010) %>%
-  select(state = STATEA, county = COUNTYA, tract = TRACTA, block = BLOCKA,
+  select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block = BLOCKA,
          aian, api, black, hispanic, other, white)
 
-for (state_code in unique(blocks_1990$state)) {
+for (state in unique(blocks_1990$state_code)) {
   blocks_1990 %>%
-    filter(state == state_code) %>%
-    write_csv(here("data", "1990", paste0("blocks_1990_", state_code, ".csv")))
+    filter(state_code == state) %>%
+    write_csv(here("data", "1990", paste0("blocks_1990_", state, ".csv")))
 }
 
 
@@ -31,13 +31,13 @@ blocks_2000 <- read_csv(here("data-raw", "nhgis_ds147_2000_block.csv")) %>%
          other = FYF006 + FYF007,
          hispanic = FYF008 + FYF009 + FYF010 + FYF011 + FYF012 + FYF013 + FYF014,
          block = as.character(BLOCKA)) %>%
-  select(state = STATEA, county = COUNTYA, tract = TRACTA, block,
+  select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block,
          aian, api, black, hispanic, other, white)
 
-for (state_code in unique(blocks_2000$state)) {
+for (state in unique(blocks_2000$state_code)) {
   blocks_2000 %>%
-    filter(state == state_code) %>%
-    write_csv(here("data", "2000", paste0("blocks_2000_", state_code, ".csv")))
+    filter(state_code == state) %>%
+    write_csv(here("data", "2000", paste0("blocks_2000_", state, ".csv")))
 }
 
 
@@ -51,11 +51,11 @@ blocks_2010 <- read_csv(here("data-raw", "nhgis_ds172_2010_block.csv")) %>%
          other = H7Z008 + H7Z009,
          hispanic = H7Z010,
          block = as.character(BLOCKA)) %>%
-  select(state = STATEA, county = COUNTYA, tract = TRACTA, block,
+  select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block,
          aian, api, black, hispanic, other, white)
 
-for (state_code in unique(blocks_2010$state)) {
+for (state in unique(blocks_2010$state_code)) {
   blocks_2010 %>%
-    filter(state == state_code) %>%
-    write_csv(here("data", "2010", paste0("blocks_2010_", state_code, ".csv")))
+    filter(state_code == state) %>%
+    write_csv(here("data", "2010", paste0("blocks_2010_", state, ".csv")))
 }
