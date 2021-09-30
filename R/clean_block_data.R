ec0c7f2c3dfa839650bac2e27e4d68ff61d0ea27
Bertrand Wilden <bert@bwilden.com>
2021-09-30 15:54:44 -0700

    api to aapi and added multi

--- R/clean_block_data.R
+++ R/clean_block_data.R
@@ -8,11 +8,11 @@ blocks_1990 <- read_csv(here("data-raw", "nhgis_ds120_1990_block.csv")) %>%
   mutate(white = ET2001,
          black = ET2002,
          aian = ET2003,
-         api = ET2004,
+         aapi = ET2004,
          other = ET2005,
          hispanic = ET2006 + ET2007 + ET2008 + ET2009 + ET2010) %>%
   select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block = BLOCKA,
-         aian, api, black, hispanic, other, white)
+         aapi, aian, black, hispanic, other, white)
 
 for (state in unique(blocks_1990$state_code)) {
   blocks_1990 %>%
@@ -27,12 +27,13 @@ blocks_2000 <- read_csv(here("data-raw", "nhgis_ds147_2000_block.csv")) %>%
   mutate(white = FYF001,
          black = FYF002,
          aian = FYF003,
-         api = FYF004 + FYF005,
-         other = FYF006 + FYF007,
-         hispanic = FYF008 + FYF009 + FYF010 + FYF011 + FYF012 + FYF013 + FYF014,
+         aapi = FYF004 + FYF005,
+         other = FYF006,
+         multi = FYF007 + FYF014,
+         hispanic = FYF008 + FYF009 + FYF010 + FYF011 + FYF012 + FYF013,
          block = as.character(BLOCKA)) %>%
   select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block,
-         aian, api, black, hispanic, other, white)
+         aapi, aian, black, hispanic, multi, other, white)
 
 for (state in unique(blocks_2000$state_code)) {
   blocks_2000 %>%
@@ -47,12 +48,13 @@ blocks_2010 <- read_csv(here("data-raw", "nhgis_ds172_2010_block.csv")) %>%
   mutate(white = H7Z003,
          black = H7Z004,
          aian = H7Z005,
-         api = H7Z006 + H7Z007,
-         other = H7Z008 + H7Z009,
-         hispanic = H7Z010,
+         aapi = H7Z006 + H7Z007,
+         other = H7Z008,
+         multi = H7Z009 + H7Z017,
+         hispanic = H7Z010 - H7Z017,
          block = as.character(BLOCKA)) %>%
   select(state_code = STATEA, county = COUNTYA, tract = TRACTA, block,
-         aian, api, black, hispanic, other, white)
+         aapi, aian, black, hispanic, multi, other, white)
 
 for (state in unique(blocks_2010$state_code)) {
   blocks_2010 %>%
